97a91aeeaf83f7b062209033b4f663cd12297320
Hamza Koc <hamzakooc@gmail.com>
2022-04-05 13:00:49 -0400

    read data from REST API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Mission } from './models/mission';
+import { SpacexapiService } from './network/spacexapi.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = '101274109-lab-test2-comp3133';
+  missionList?: Mission;
+  isReady?: true;
+
+  constructor(private spacexapiService: SpacexapiService) {
+    this.getMissionData();
+  }
+
+  getMissionData() {
+    this.spacexapiService.getAllMissions().subscribe((data) => {
+      console.log(data);
+      this.missionList = data;
+    });
+  }
+
+  
 }
